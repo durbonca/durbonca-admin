3eed71bd271f1cbe125f84e8d80035793a524128
Manuel Duran <durbonca@gmail.com>
2022-01-18 13:26:05 -0300

    fucking dependencies

--- src/App.js
+++ src/App.js
@@ -5,31 +5,16 @@ import {
   FirebaseAuthProvider,
   FirebaseDataProvider,
 } from 'react-admin-firebase';
+import config from './utils/firebaseConfig';
 
-const {
-  REACT_APP_FIREBASE_API_KEY,
-  REACT_APP_FIREBASE_AUTH_DOMAIN,
-  REACT_APP_FIREBASE_PROJECT_ID,
-  REACT_APP_FIREBASE_STORAGE_BUCKET,
-  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  REACT_APP_FIREBASE_APP_ID
- } = process.env;
-
-const config = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: REACT_APP_FIREBASE_APP_ID,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-};
-
-const options = {};
+const options = { logging: true };
 
 const dataProvider = FirebaseDataProvider(config, options);
-const authProvider = FirebaseAuthProvider(config);
-const App = () => <Admin dataProvider={dataProvider} authProvider={authProvider} >
+const authProvider = FirebaseAuthProvider(config, options);
+const App = () => (
+<Admin dataProvider={dataProvider} authProvider={authProvider} >
   <Resource name="projects" list={ListGuesser} />
-</Admin>;
+</Admin>
+);
 
 export default App;
